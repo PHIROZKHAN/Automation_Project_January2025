0eeb88d4afb1594bd41c184beb157f79a5cfc4b7
Phiroz khan <phiroz679@gmail.com>
2025-01-09 14:15:42 +0530

    added new test page

---
+++ src/test/java/test/Text_box_test3.java
@@ -0,0 +1,53 @@
+package test;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Baseutility.BaseLibrary;
+import pages.Text_box_page;
+
+public class Text_box_test3 extends BaseLibrary {
+	Text_box_page ob;
+	@BeforeTest
+	public void launcher()
+	{
+		launchUrl();
+	ob=new Text_box_page();
+	
+	}
+	@AfterMethod
+	public void flush()
+	{
+		driver.close();
+	}
+	@Test(priority=2 , enabled=true)
+	public void clickAtPractice() {
+	
+		ob.clickOnPractice();
+	}
+    @Test(priority=1 , enabled=true)
+    public void clickAtClose()
+    {
+    	ob.clickOnClose();
+    }
+	@Test(priority=3 ,enabled=true)
+	public void clickAtElement()
+	{
+		ob.clickOnElement();
+	}
+	@Test(priority=4, enabled=true)
+	public void clickAtBox()
+	{
+		ob.clickOnTextBox();
+	}
+	@Test(priority=5 , enabled=true)
+	public void clickAtNmae()
+	{
+		ob.clickOnNameBox();
+	}
+	
+
+	
+	
+}
